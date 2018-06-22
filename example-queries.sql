bc428af59f0d0b89424ad68cf4cc075c336c4912
Thor Denson <tjdenson@eckerd.edu>
2018-06-22 11:01:36 -0400

    added text file

---
+++ example-queries.sql
@@ -0,0 +1,45 @@
+--get all the todos
+SELECT * FROM todos;
+
+	
+--get one todo by id
+SELECT * FROM todos
+	WHERE id=2;
+	
+
+--get all the pending todos
+SELECT * FROM todos
+	WHERE isDone=false;
+
+--get all the completed todos
+SELECT * FROM todos
+	WHERE isDone=true;
+
+--search by title, should have 0 results
+SELECT * FROM todos
+	WHERE title ilike '%zzzzzz%';
+
+
+--search by title, should have 1 result
+SELECT * FROM todos
+	WHERE title ilike '%scoop%';
+	
+--"uncheck" a todo
+UPDATE todos
+    SET isDone= false
+    WHERE id=1;
+
+--"check off" a todo
+UPDATE todos
+    SET isDone= true
+    WHERE id=2;
+
+--change title of a todo
+UPDATE todos
+    SET title='cook amazing dinner'
+    WHERE id=3;
+
+
+	
+
+	
